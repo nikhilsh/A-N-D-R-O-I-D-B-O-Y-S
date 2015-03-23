d0baa82c3b2e04781db5a73b5e72d9a9fffe0959
Hao Qin Tan <haoqintan@Haos-MacBook-Air.local>
2015-03-23 21:35:53 +0800

    Added Android Studio game files

---
+++ android/src/com/androidboys/spellarena/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.androidboys.spellarena.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.androidboys.spellarena.game.SpellArena;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpellArena(), config);
+	}
+}
