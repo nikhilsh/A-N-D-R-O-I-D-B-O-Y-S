b88dc6fbe45c303e41e6c44373db02a255e0f143
Hello World <admin@hwlyz.com>
2015-04-01 22:19:39 +0800

    doc comments

--- core/src/com/androidboys/spellarena/helper/InputHandler.java
+++ core/src/com/androidboys/spellarena/helper/InputHandler.java
@@ -35,47 +35,96 @@ public class InputHandler implements InputProcessor{
 		Gdx.input.setInputProcessor(im);
 	}
 	
+	/**
+	 * Called when a key was pressed.
+	 * @param keycode - one of the constants in Input.Keys
+	 * @return whether the input was processed
+	 */
 	@Override
 	public boolean keyDown(int keycode) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * Called when a key was released.
+	 * @param keycode - one of the constants in Input.Keys
+	 * @return whether the input was processed
+	 */
 	@Override
 	public boolean keyUp(int keycode) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * Called when a key was typed.
+	 * @param keycode - one of the constants in Input.Keys
+	 * @return whether the input was processed
+	 */
 	@Override
 	public boolean keyTyped(char character) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * Called when the screen was touched or a mouse button was pressed.
+	 * @param screenX - The x coordinate, origin is in the upper left corner
+	 * @param screenY - The y coordinate, origin is in the upper left corner
+	 * @param pointer - the pointer for the event.
+	 * @param button - the button
+	 * @return whether the input was processed
+	 */
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 		return false;
 	}
 
+	/**
+	 * Called when a finger was lifted or a mouse button was released.
+	 * @param screenX - The x coordinate, origin is in the upper left corner
+	 * @param screenY - The y coordinate, origin is in the upper left corner
+	 * @param pointer - the pointer for the event.
+	 * @param button - the button
+	 * @return whether the input was processed
+	 */
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * Called when a finger or the mouse was dragged.
+	 * @param screenX - The x coordinate, origin is in the upper left corner
+	 * @param screenY - The y coordinate, origin is in the upper left corner
+	 * @param pointer - the pointer for the event.
+	 * @return whether the input was processed
+	 */
 	@Override
 	public boolean touchDragged(int screenX, int screenY, int pointer) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * Called when the mouse was moved without any buttons being pressed.
+	 * @param screenX - The x coordinate, origin is in the upper left corner
+	 * @param screenY - The y coordinate, origin is in the upper left corner
+	 * @return whether the input was processed
+	 */
 	@Override
 	public boolean mouseMoved(int screenX, int screenY) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * Called when the mouse wheel was scrolled.
+	 * @param amount - the scroll amount, -1 or 1 depending on the direction the wheel was scrolled.
+	 * @return whether the input was processed
+	 */
 	@Override
 	public boolean scrolled(int amount) {
 		// TODO Auto-generated method stub
